51c40b86721ba7a6a2289d09937f353f412f45de
gitkraken303 <johnspaxxvile@gmail.com>
2025-10-24 13:32:13 +0300

    fixing loader on learn

--- src/components/LearnFeed.jsx
+++ src/components/LearnFeed.jsx
@@ -25,6 +25,7 @@ const LearnFeed = () => {
       setLoading(true);
       const { data } = await axios.get("/courses/approved");
       setCoursesFeed(data);
+      setLoading(false);
     } catch {
       toast.error("Error fetching courses");
     } finally {
@@ -94,20 +95,28 @@ const LearnFeed = () => {
           <AiOutlineArrowUp />
         </div>
       )}
-      {/* Search Bar */}
-      <div className="flex items-center bg-gray-100 px-3 py-2 rounded-md w-full md:w-1/3 mb-6">
-        <AiOutlineSearch className="text-lg mr-2 text-gray-500" />
-        <input
-          type="text"
-          placeholder="Search course..."
-          className="bg-transparent outline-none w-full"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-      </div>
 
-      {/* Categories */}
-      {/* <div className="flex flex-wrap gap-8 mb-8">
+      {loading ? (
+        <div className="mt-[5em] flex justify-center">
+          <p className="text-[#0067b8] font-semibold text-xl">
+            Loading Courses ...
+          </p>
+        </div>
+      ) : (
+        <>
+          {/* Search Bar */}
+          <div className="flex items-center bg-gray-100 px-3 py-2 rounded-md w-full md:w-1/3 mb-6">
+            <AiOutlineSearch className="text-lg mr-2 text-gray-500" />
+            <input
+              type="text"
+              placeholder="Search course..."
+              className="bg-transparent outline-none w-full"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+          </div>
+          {/* Categories */}
+          {/* <div className="flex flex-wrap gap-8 mb-8">
         {CourseCategories?.map((item) => (
           <span
             key={item}
@@ -122,109 +131,115 @@ const LearnFeed = () => {
           </span>
         ))}
       </div> */}
-      {/* Left Arrow */}
-      {/* <button
+          {/* Left Arrow */}
+          {/* <button
         onClick={() => scroll("left")}
         className="absolute left-0 bg-white shadow-md rounded-full p-2 z-10 hidden sm:block"
       >
         <FaArrowLeft className="w-5 h-5 text-gray-600" />
       </button> */}
 
-      {/* Scrollable Categories */}
-      <div
-        ref={scrollRef}
-        className="flex gap-8 overflow-x-auto scrollbar-hide px-10 scroll-smooth"
-        style={{ scrollBehavior: "smooth" }}
-      >
-        {CourseCategories?.map((item) => (
-          <span
-            key={item}
-            className={`cursor-pointer whitespace-nowrap transition-all ${
-              selectedCategory === item
-                ? "text-[#0067b8] font-bold border-b-4 border-[#0067b8] pb-1"
-                : "text-gray-600 hover:text-[#0067b8]"
-            }`}
-            onClick={() => setSelectedCategory(item)}
+          {/* Scrollable Categories */}
+          <div
+            ref={scrollRef}
+            className="flex gap-8 overflow-x-auto scrollbar-hide px-10 scroll-smooth"
+            style={{ scrollBehavior: "smooth" }}
           >
-            {item}
-          </span>
-        ))}
-      </div>
+            {CourseCategories?.map((item) => (
+              <span
+                key={item}
+                className={`cursor-pointer whitespace-nowrap transition-all ${
+                  selectedCategory === item
+                    ? "text-[#0067b8] font-bold border-b-4 border-[#0067b8] pb-1"
+                    : "text-gray-600 hover:text-[#0067b8]"
+                }`}
+                onClick={() => setSelectedCategory(item)}
+              >
+                {item}
+              </span>
+            ))}
+          </div>
 
-      {/* Right Arrow */}
-      {/* <button
+          {/* Right Arrow */}
+          {/* <button
         onClick={() => scroll("right")}
         className="absolute right-0 bg-white shadow-md rounded-full p-2 z-10 hidden sm:block"
       >
         <FaArrowRight className="w-5 h-5 text-gray-600" />
       </button> */}
 
-      {/* Feed */}
-      {filteredCourses.length === 0 ? (
-        <p className="text-gray-600 text-center mt-8 text-lg">
-          No course found.
-        </p>
-      ) : (
-        <div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8"
-          style={{ marginTop: "2em" }}
-        >
-          {filteredCourses.map((course, index) => (
+          {/* Feed */}
+          {filteredCourses.length === 0 ? (
+            <p className="text-gray-600 text-center mt-8 text-lg">
+              No course found.
+            </p>
+          ) : (
             <div
-              key={index}
-              className="hover:shadow-lg p-2 rounded-xl transition"
+              className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8"
+              style={{ marginTop: "2em" }}
             >
-              <a href={course.link} target="_blank" rel="noopener noreferrer">
-                <div className="w-full h-64 sm:h-72 md:h-80 flex items-center justify-center bg-gray-100 rounded-xl overflow-hidden">
-                  <img
-                    src={course.image}
-                    alt={course.title}
-                    className="w-full h-full object-cover rounded-xl"
-                  />
-                </div>
+              {filteredCourses.map((course, index) => (
+                <div
+                  key={index}
+                  className="hover:shadow-lg p-2 rounded-xl transition"
+                >
+                  <a
+                    href={course.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="w-full h-64 sm:h-72 md:h-80 flex items-center justify-center bg-gray-100 rounded-xl overflow-hidden">
+                      <img
+                        src={course.image}
+                        alt={course.title}
+                        className="w-full h-full object-cover rounded-xl"
+                      />
+                    </div>
 
-                <div className="mt-4">
-                  <p className="text-[#0067b8] text-sm font-bold mb-3">
-                    {course.tag}
-                  </p>
-                  <h2 className="font-bold mb-3">{course.title}</h2>
-                  <p className="text-gray-600 text-sm">
-                    {course.desc.length > 100
-                      ? course.desc.substring(0, 100) + "..."
-                      : course.desc}
-                  </p>
-                  <div className="mt-4">
-                    {course.organization === "kepsa" ? (
-                      <img
-                        src={kepsa}
-                        alt={course.organization}
-                        height={20}
-                        width={30}
-                        className="rounded-md"
-                      />
-                    ) : course.organization === "undp" ? (
-                      <img
-                        src={Undp}
-                        alt={course.organization}
-                        height={20}
-                        width={30}
-                        className="rounded-md"
-                      />
-                    ) : course.organization === "microsoft" ? (
-                      <img
-                        src={Mic}
-                        alt={course.organization}
-                        height={20}
-                        width={30}
-                        className="rounded-md"
-                      />
-                    ) : null}
-                  </div>
+                    <div className="mt-4">
+                      <p className="text-[#0067b8] text-sm font-bold mb-3">
+                        {course.tag}
+                      </p>
+                      <h2 className="font-bold mb-3">{course.title}</h2>
+                      <p className="text-gray-600 text-sm">
+                        {course.desc.length > 100
+                          ? course.desc.substring(0, 100) + "..."
+                          : course.desc}
+                      </p>
+                      <div className="mt-4">
+                        {course.organization === "kepsa" ? (
+                          <img
+                            src={kepsa}
+                            alt={course.organization}
+                            height={20}
+                            width={30}
+                            className="rounded-md"
+                          />
+                        ) : course.organization === "undp" ? (
+                          <img
+                            src={Undp}
+                            alt={course.organization}
+                            height={20}
+                            width={30}
+                            className="rounded-md"
+                          />
+                        ) : course.organization === "microsoft" ? (
+                          <img
+                            src={Mic}
+                            alt={course.organization}
+                            height={20}
+                            width={30}
+                            className="rounded-md"
+                          />
+                        ) : null}
+                      </div>
+                    </div>
+                  </a>
                 </div>
-              </a>
+              ))}
             </div>
-          ))}
-        </div>
+          )}
+        </>
       )}
     </div>
   );
